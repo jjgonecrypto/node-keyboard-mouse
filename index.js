03db2d10ae03397cf7a7ea1b2bfc98b06aab0288
justin j. moses <justinjmoses@gmail.com>
2016-11-26 14:12:02 -0500

    Adding base requirement

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 'use strict'
 
-module.exports = {}
+const Rx = require('node-keyboard-rx')()
+
+const mouseEvents = require('osx-mouse')()
+
+module.exports = {
+    serialism({ w, h } = { w: 2560, h: 1440 }) {
+        const mouseMove = Rx.Observable
+            .fromEvent(mouseEvents, 'move', (x, y) => {
+                return { x: Math.ceil(x), y: Math.ceil(y) }
+            })
+
+        const throttledMidiNotes = mouseMove
+            .throttle(({ x }) => Rx.Observable.timer(x / w * 1000))
+            .map(({ y }) => Math.min(Math.round(88 * y / h) + 21, 109) )
+
+        return throttledMidiNotes
+    }
+}
